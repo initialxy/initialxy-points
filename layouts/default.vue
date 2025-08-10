930ce81191be1d8d8c4d648f26c2ffad1d195ea7
initialxy <initialxy@gmail.com>
2025-08-10 15:57:16 -0700

    Added an entry point to password change

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,11 +10,33 @@
 
     <div class="fixed bottom-4 left-4 z-10 overflow-hidden">
       <UButton
+        v-if="isMoreExpanded"
+        icon="i-lucide-square-asterisk"
+        color="info"
+        variant="solid"
+        size="xl"
+        @click="changePassword"
+        block
+        class="w-10 h-10 rounded-full flex my-4"
+        :ui="{leadingIcon: 'text-lg'}"
+      />
+      <UButton
+        v-if="isMoreExpanded"
         icon="i-lucide-log-out"
         color="error"
         variant="solid"
         size="xl"
         @click="logoutClicked"
+        block
+        class="w-10 h-10 rounded-full flex my-4"
+        :ui="{leadingIcon: 'text-lg'}"
+      />
+      <UButton
+        icon="i-lucide-menu"
+        color="secondary"
+        variant="solid"
+        size="xl"
+        @click="toggleMore"
         block
         class="w-10 h-10 rounded-full flex"
         :ui="{leadingIcon: 'text-lg'}"
@@ -27,10 +49,19 @@
 const { loggedIn } = useUserSession()
 const store = useStore()
 
+const isMoreExpanded = ref(false)
+
+const toggleMore = async () => {
+  isMoreExpanded.value = !isMoreExpanded.value
+}
+
 const logoutClicked = async () => {
   await store.logout()
   await sleep(200)
   await navigateTo('/login')
+}
+
+const changePassword = async () => {
 }
 </script>
 
