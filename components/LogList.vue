55c801f1f266511eab05f8594aee3604fa3c5e99
initialxy <initialxy@gmail.com>
2025-08-10 03:33:27 -0700

    Use timeline instead

--- components/LogList.vue
+++ components/LogList.vue
@@ -1,19 +1,69 @@
 <template>
-  <div class="max-w-100 mx-auto">
-    <UCard v-for="log in props.logs" :key="log.id" class="my-4">
-      <p class="text-sm text-neutral-400 dark:text-neutral-600">
-        <span v-if="log.timestamp">{{ formatShortDate(log.timestamp) }}</span>
-        <span v-else>Unknown time</span>
-      </p>
-      <p>
-        <FormattedLog :log="log" />
-      </p>
-    </UCard>
-  </div>
+  <UTimeline
+    :items="items"
+    size="xs"
+    :ui="{
+      date: 'float-end ms-1',
+      description: 'px-3 py-2 ring ring-default mt-2 rounded-md text-default',
+    }"
+    class="max-w-100 mx-auto"
+  >
+    <template #title="{ item }">
+      <span>{{ item.username }}</span>
+      <span class="font-normal text-muted">&nbsp;{{ item.action }}</span>
+    </template>
+
+    <template #date="{ item }">
+      {{ useTimeAgo(new Date(item.date || '')) }}
+    </template>
+  </UTimeline>
 </template>
 
 <script setup lang="ts">
+import type { TimelineItem } from '@nuxt/ui'
+
 const props = defineProps<{
   logs: Log[]
 }>()
+
+const { data: allUsers } = await useFetch<UsersResponse>('/api/users', {
+  query: { role: 'all' },
+})
+
+const getUserById = (id: number): User | null => {
+  return allUsers?.value?.users.find((user) => user.id === id) || null
+}
+
+const getUsername = (userId: number): string => {
+  const user = getUserById(userId)
+  return user ? user.username : ''
+}
+
+const items: TimelineItem[] = props.logs.map((log) => {
+  const username = getUsername(log.actor_id)
+  const actionType = log.action_type
+  const date = new Date(log.timestamp || 0).toISOString()
+
+  switch (actionType) {
+    case 'change_points':
+      const pointsChange = (log.points_after || 0) - (log.points_before || 0)
+      const action = `${pointsChange > 0 ? 'added' : 'reduced'} ${Math.abs(pointsChange)} points to ${getUsername(log.recipient_id || 0)}`
+      const icon =
+        pointsChange > 0 ? 'i-lucide-arrow-up' : 'i-lucide-arrow-down'
+      const description = `${getUsername(log.recipient_id || 0)} now has ${log.points_after} points`
+      return {
+        username,
+        date,
+        action,
+        icon,
+        description,
+      }
+  }
+
+  return {
+    username,
+    date,
+    action: actionType,
+  }
+})
 </script>
--- server/api/logs/index.get.ts
+++ server/api/logs/index.get.ts
@@ -8,24 +8,23 @@ export default defineEventHandler(async (event: H3Event) => {
   const session = await requireUserSession(event)
   const user = session.user as User
   const { limit } = getQuery(event)
-  
-  const numLimit = parseInt(limit as string || DEFAULT_LIMIT)
-  const cond = user.role === 'parent'
-    ? '1 = 1'  // Dummy condition
-    : 'recipient_id = ?'
-  
+
+  const numLimit = parseInt((limit as string) || DEFAULT_LIMIT)
+  const cond =
+    user.role === 'parent'
+      ? '1 = 1' // Dummy condition
+      : 'recipient_id = ?'
+
   const logs: Log[] = await db.all(
-      `
+    `
       SELECT id, CAST(strftime('%s', timestamp) AS INTEGER) * 1000 AS timestamp, actor_id, action_type, recipient_id, points_before, points_after, additional_context
       FROM logs
       WHERE ${cond}
       ORDER BY timestamp DESC
       LIMIT ?
       `,
-      user.role === 'parent'
-        ? [numLimit]
-        : [user.id, numLimit]
-    );
+    user.role === 'parent' ? [numLimit] : [user.id, numLimit]
+  )
 
   return { logs } as LogsResponse
 })
