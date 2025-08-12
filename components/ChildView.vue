a75e172170df4873f2e12aa22a1ef326ff36ac4e
initialxy <initialxy@gmail.com>
2025-08-11 17:24:56 -0700

    Better styling for child card

--- components/ChildView.vue
+++ components/ChildView.vue
@@ -2,14 +2,16 @@
   <div class="max-w-100 mx-auto">
     <UCard class="mb-4" variant="subtle">
       <h1 class="text-3xl font-bold text-center mb-4">
-        <span class="text-info">{{ user?.username }}</span> earned
+        <span class="text-secondary">{{ user?.username }}</span>
+        <span class="text-neutral-500"> earned</span>
       </h1>
       <div class="text-6xl font-extrabold text-primary text-center mb-4">
         {{ user?.points || 0 }}
+        <span class="text-neutral-500 text-base absolute ml-2 mt-8">pts</span>
       </div>
-      <p class="text-xl text-center opacity-50">
-        points. Keep it up!
+      <p class="text-xl text-center text-neutral-500">
         <UIcon name="i-lucide-thumbs-up" />
+        Keep it up!
       </p>
     </UCard>
   </div>
